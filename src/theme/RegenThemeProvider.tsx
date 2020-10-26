831a9e56aa29d96829662483ac09f9141546fc74
blushi <marie.gauthier63@gmail.com>
2020-10-26 15:13:44 +0100

    Delete unused files

--- src/theme/RegenThemeProvider.tsx
+++ src/theme/RegenThemeProvider.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 import * as React from 'react';
 import theme from './muiTheme';
-import 'fonts.css';
 
 interface Props {
+  readonly injectFonts?: boolean;
   readonly injectStyles?: (props?: any) => void;
   readonly children: React.ReactNode;
 }
 
-const MedulasThemeProvider = ({ injectStyles, children }: Props): JSX.Element => {
+const MedulasThemeProvider = ({ injectFonts = true, injectStyles, children }: Props): JSX.Element => {
   if (injectStyles) {
     injectStyles();
+  }
+
+  if (injectFonts) {
+    require('./fonts.css');
   }
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
