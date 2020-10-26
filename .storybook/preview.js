b1f6f365b120008ebc26cc1ad3a3122a08a2edca
blushi <marie.gauthier63@gmail.com>
2020-10-26 10:42:54 +0100

    Add storybook addon material ui

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
 
+import theme from '../src/theme/muiTheme';
+import { muiTheme } from 'storybook-addon-material-ui';
+import '../src/theme/fonts.css';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+    },
+  },
 }
+
+export const decorators = [
+  muiTheme([theme]), 
+];
